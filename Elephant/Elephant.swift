fa29f34b627c5afa6fa07192a168bc89f30fe688
Stepan Hruda <stepan.hruda@gmail.com>
2015-12-05 17:21:31 -0500

    Add basic connection and tests for it

--- Elephant/Elephant.swift
+++ Elephant/Elephant.swift
@@ -1,4 +1,90 @@
 import libpq
 
-func connect() {
+struct ConnectionParameters {
+    let host: String
+    let port: String
+    let options: String
+    let tty: String
+    let databaseName: String
+    let login: String
+    let password: String
 }
+
+enum ConnectionError: ErrorType {
+    case ConnectionFailed
+}
+
+class Database {
+    static func connect(parameters: ConnectionParameters) throws -> Connection {
+
+        let connectionPointer = PQsetdbLogin(parameters.host,
+            parameters.port,
+            parameters.options,
+            parameters.tty,
+            parameters.databaseName,
+            parameters.login,
+            parameters.password)
+
+        guard PQstatus(connectionPointer) == CONNECTION_OK else { throw ConnectionError.ConnectionFailed }
+
+        return Connection(pointer: connectionPointer)
+    }
+}
+
+enum QueryError: ErrorType {
+    case InvalidQuery
+}
+
+public class Connection {
+    let pointer: COpaquePointer
+
+    init(pointer: COpaquePointer) {
+        self.pointer = pointer
+    }
+
+    func execute(query: Query) throws -> QueryResult {
+        let resultPointer = PQexec(pointer, query.string)
+
+        let status = PQresultStatus(resultPointer)
+
+        switch status {
+        case PGRES_COMMAND_OK, PGRES_TUPLES_OK: break
+        default: throw QueryError.InvalidQuery
+        }
+
+        return QueryResult(pointer: resultPointer)
+    }
+}
+
+public final class Query: StringLiteralConvertible {
+    let string: String
+
+    public typealias StringLiteralType = String
+    public typealias ExtendedGraphemeClusterLiteralType = String
+    public typealias UnicodeScalarLiteralType = String
+
+    public required init(string: String) {
+        self.string = string
+    }
+
+    public convenience init(stringLiteral value: String) {
+        self.init(string: value)
+    }
+
+    public convenience init(unicodeScalarLiteral value: String) {
+        self.init(string: value)
+    }
+
+    public convenience init(extendedGraphemeClusterLiteral value: String) {
+        self.init(string: value)
+    }
+}
+
+struct QueryResult {
+    let pointer: COpaquePointer
+
+    init(pointer: COpaquePointer) {
+        self.pointer = pointer
+    }
+}
+
--- ElephantTests/ElephantTests.swift
+++ ElephantTests/ElephantTests.swift
@@ -6,8 +6,68 @@ class ElephantTests: QuickSpec {
 
     override func spec() {
 
-        it("runs") {
-            expect(false).to(beTrue())
+        let validDatabaseParameters = ConnectionParameters(
+            host: "192.168.99.100",
+            port: "32768",
+            options: "",
+            tty: "",
+            databaseName: "elephant",
+            login: "postgres",
+            password: "")
+
+        describe("Connection") {
+
+            context("with valid connection parameters") {
+                it("returns a connection") {
+
+                    expect {
+                        try Database.connect(validDatabaseParameters)
+                        return nil
+                        }.toNot(throwError())
+                }
+            }
+
+            context("with invalid connection parameters") {
+                it("throws a connection failed error") {
+
+                    expect {
+
+                        let parameters = ConnectionParameters(
+                            host: "",
+                            port: "",
+                            options: "",
+                            tty: "",
+                            databaseName: "",
+                            login: "",
+                            password: "")
+                        
+                        try Database.connect(parameters)
+                        return nil
+                        }.to(throwError(errorType: ConnectionError.self))
+                }
+            }
+        }
+
+        describe("Query") {
+            let connection = try! Database.connect(validDatabaseParameters)
+
+            context("when executing a valid query") {
+                it("doesn't throw an error") {
+                    expect {
+                        try connection.execute("SELECT 1;")
+                        return nil
+                    }.toNot(throwError())
+                }
+            }
+
+            context("when executing an invalid query") {
+                it("throws an invalid query error") {
+                    expect {
+                        try connection.execute("OH MY GOD;")
+                        return nil
+                        }.to(throwError(errorType: QueryError.self))
+                }
+            }
         }
     }
 }
