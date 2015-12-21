f7aa1c649691e830fadf806720ef308fc23cd113
Avrohom Katz <iambpentameter@gmail.com>
2015-12-21 12:19:48 -0500

    Fix preprocessor macro and add gitignore

--- Package.swift
+++ Package.swift
@@ -1,13 +1,17 @@
 import PackageDescription
 
+#if os(Linux)
 let package = Package(
   name: "PostgreSQL",
   dependencies: [
-#if os(Linux)
     .Package(url: "https://github.com/stepanhruda/libpq.git", majorVersion: 9)
-#else
-    .Package(url: "https://github.com/stepanhruda/libpq-darwin.git", majorVersion: 9)
-#endif
   ]
 )
-
+#else
+let package = Package(
+  name: "PostgreSQL",
+  dependencies: [
+    .Package(url: "https://github.com/stepanhruda/libpq-darwin.git", majorVersion: 9)
+  ]
+)
+#endif
