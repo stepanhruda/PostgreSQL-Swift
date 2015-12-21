50ca27e2d0dbd45cf6ad8d77f1aaecdb3160863f
Stepan Hruda <stepan.hruda@gmail.com>
2015-12-21 12:08:56 -0500

    Add Darwin specific modulemap

--- Package.swift
+++ Package.swift
@@ -3,7 +3,11 @@ import PackageDescription
 let package = Package(
   name: "PostgreSQL",
   dependencies: [
+#if os(Linux)
     .Package(url: "https://github.com/stepanhruda/libpq.git", majorVersion: 9)
+#else
+    .Package(url: "https://github.com/stepanhruda/libpq-darwin.git", majorVersion: 9)
+#endif
   ]
 )
 
