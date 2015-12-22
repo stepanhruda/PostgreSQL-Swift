06321d1462af54a9a55ec2203fae8305371cd9df
Stepan Hruda <stepan.hruda@gmail.com>
2015-12-22 15:15:21 -0500

    Make subscript public

--- Sources/QueryResultRow.swift
+++ Sources/QueryResultRow.swift
@@ -2,7 +2,7 @@ public struct QueryResultRow {
     public let columnValues: [Any?]
     unowned let queryResult: QueryResult
 
-    subscript(columnName: String) -> Any? {
+    public subscript(columnName: String) -> Any? {
         get {
             guard let index = queryResult.columnIndexesForNames[columnName] else { return nil }
             return columnValues[index]
